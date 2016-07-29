2a72e24e2e4a1b8648c91e1fb9b46241a8a27fb1
Raghav <sragha45@gmail.com>
2016-07-30 00:41:51 +0530

    Usage of Chords to open new tabs.

--- src/Sample.java
+++ src/Sample.java
@@ -15,28 +15,35 @@ import java.util.List;
 public class Sample {
     public static void main(String[] args) throws IOException {
 //        System.setProperty("webdriver.chrome.driver","/home/psg/selenium-2.53.1/chromedriver");
+
         WebDriver driver = new FirefoxDriver();
         driver.get("http://ecampus.psgtech.ac.in/");
 
         driver.findElement(By.className("current")).click();
         driver.findElement(By.name("Txtstudid")).sendKeys("14i339");
         driver.findElement(By.name("TxtPasswd")).sendKeys("26jul96");
-
         driver.findElement(By.name("btnlogin")).click();
 
         Actions action = new Actions(driver);
         WebElement element = driver.findElement(By.xpath("//*[@id=\"Title1_Menu1-menuItem002\"]"));
         action.moveToElement(element).moveToElement(driver.findElement
-                (By.xpath("//*[@id=\"Title1_Menu1-menuItem002-subMenu-menuItem003\"]"))).click().build().perform();
+                (By.xpath("//*[@id=\"Title1_Menu1-menuItem002-subMenu-menuItem003\"]")))
+                .click().build().perform();
 
-        // When using Whatsapp Web Hack use the following to get the id.
-        // driver.find_elements_by_xpath("//*[contains(text(), 'My Button')]")
+
+        //Take a screenshot and save it in home folder.
+
         File screenShot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-//        FileUtils.copyFile(screenShot,new File("/home/psg/screenshot.png"));
+        FileUtils.copyFile(screenShot,new File("/home/psg/screenshot.png"));
 
         //*[@id="PDGcourpercView"]/tbody/tr[8]/td[7]
-        String selectLinkOpeninNewTab = Keys.chord(Keys.CONTROL,"t");
-        driver.findElement(By.tagName("body")).sendKeys(selectLinkOpeninNewTab);
+
+
+
+        //Hold down the keys "Control" and "T" to open a new tab and open stackoverflow.com
+
+        String selectLinkOpenInNewTab = Keys.chord(Keys.CONTROL,"t");
+        driver.findElement(By.tagName("body")).sendKeys(selectLinkOpenInNewTab);
         driver.get("stackoverflow.com");
 
     }
