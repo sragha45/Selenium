653914e17d7bdc26d8e21c651041a3974d49d0a4
Raghav <sragha45@gmail.com>
2016-07-29 23:28:42 +0530

    Takes screenshot of My Attendance and saves in the home folder Display all the set of possible links

--- src/Sample.java
+++ src/Sample.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.interactions.Actions;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by psg on 29/7/16.
@@ -16,10 +17,13 @@ public class Sample {
 //        System.setProperty("webdriver.chrome.driver","/home/psg/selenium-2.53.1/chromedriver");
         WebDriver driver = new FirefoxDriver();
         driver.get("http://ecampus.psgtech.ac.in/");
+
         driver.findElement(By.className("current")).click();
         driver.findElement(By.name("Txtstudid")).sendKeys("14i339");
         driver.findElement(By.name("TxtPasswd")).sendKeys("26jul96");
+
         driver.findElement(By.name("btnlogin")).click();
+
         Actions action = new Actions(driver);
         WebElement element = driver.findElement(By.xpath("//*[@id=\"Title1_Menu1-menuItem002\"]"));
         action.moveToElement(element).moveToElement(driver.findElement
@@ -28,7 +32,14 @@ public class Sample {
         // When using Whatsapp Web Hack use the following to get the id.
         // driver.find_elements_by_xpath("//*[contains(text(), 'My Button')]")
         File screenShot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(screenShot,new File("/home/psg/screenshot.png"));
+//        FileUtils.copyFile(screenShot,new File("/home/psg/screenshot.png"));
+
+        List<WebElement>elements = driver.findElements(By.tagName("a"));
+        for (WebElement we :
+                elements) {
+            System.out.println(we.getText());
+
+        }
 
 
     }
